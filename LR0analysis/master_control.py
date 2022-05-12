6cce93f8c59c764eacb282f326a1df17ab4cf3df
Shine-he <hediya116@gmail.com>
2022-05-12 09:20:52 +0000

    complete LR0

--- LR0analysis/master_control.py
+++ LR0analysis/master_control.py
@@ -1,48 +1,48 @@
 def init_analysis_table():
     action = {
-        0:{
-            'a':'$',
-            'b':'$'
+        '0':{
+            'a':'B',
+            'b':'D'
         },
-        1:{
+        '1':{
             '#':'acc'
         },
-        2:{
-            'a':4
+        '2':{
+            'a':'4'
         },
-        3:{
-            'b':5
+        '3':{
+            'b':'5'
         },
-        4:{
-            'b':'$'
+        '4':{
+            'b':'B'
         },
-        5:{
-            'a':'$'
+        '5':{
+            'a':'D'
         },
-        6:{
-            'b':8
+        '6':{
+            'b':'8'
         },
-        7:{
-            'a':9
+        '7':{
+            'a':'9'
         },
-        8:{
-            '#':'BaBb'
+        '8':{
+            '#':'ABaBb'
         },
-        9:{
-            '#':'DbDa'
+        '9':{
+            '#':'ADbDa'
         }
     }
     goto = {
-        0:{
-            'A':1,
-            'B':2,
-            'D':3
+        '0':{
+            'A':'1',
+            'B':'2',
+            'D':'3'
         },
-        4:{
-            'B':6
+        '4':{
+            'B':'6'
         },
-        5:{
-            'D':7
+        '5':{
+            'D':'7'
         },
     }
     return action, goto
@@ -60,10 +60,27 @@ def analysis(status_stack, sym_stack, input_stack, *args):
     status = status_stack[-1]
     sym = sym_stack[-1]
     string = input_stack[-1]
-    if action[status][sym] < 99999999999:
-        pass
-    elif action[status][sym] == 'acc':
-        pass
+    try:
+        record = action[status][string]
+    except:
+        message('error', 'syntax error')
+    if record.isdigit():
+        status_stack.append(record)
+        sym_stack.append(string)
+        input_stack.pop()
+    elif record == 'acc':
+        return
+    else:
+        lens = len(record) - 1
+        for i in range(lens):
+            if sym_stack.pop() == record[lens-1]:
+                status_stack.pop()
+            else:
+                message('error', 'syntax error')
+        sym_stack.append(record[0])
+        status_stack.append(goto[status_stack[-1]][sym_stack[-1]])
+
+
 
 if __name__ == '__main__':
     input_string = 'ab#'
@@ -78,12 +95,20 @@ if __name__ == '__main__':
     sym_stack.append('#')
 
     status_stack = []
-    status_stack.append(0)
+    status_stack.append('0')
 
     action, goto = init_analysis_table()
 
-    analysis(status_stack, sym_stack, input_stack, action, goto)
+    while not len(input_stack) == 1:
+        print('状态栈:',status_stack)
+        print('符号栈:',sym_stack)
+        print('输入栈:',input_stack)
+        analysis(status_stack, sym_stack, input_stack, action, goto)
     
+    if len(input_stack) != 1:
+        message('error', 'syntax error')
+    else:
+        message('success','分析成功')
     
     
 
