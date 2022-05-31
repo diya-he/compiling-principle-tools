d6291b5297a78123e67f7d17fb225d0f376d7d82
Auraro <hediya116@gmail.com>
2022-05-31 23:07:21 +0800

    half complete select of the ll1

--- LL1analysis/ll1_grammar_judgment.py
+++ LL1analysis/ll1_grammar_judgment.py
@@ -1,6 +1,17 @@
+import re
+
+
 def error():
     print('不是LL1文法')
     exit(0)
+
+
+def left(head):
+    return head.split('->')[0]
+
+
+def right(head):
+    return head.split('->')[1]
 
 
 class LL1:
@@ -12,12 +23,18 @@ class LL1:
         self.isempty = {}
         self.first = {}
         self.follow = {}
+        self.select = {}
 
     def init(self):
         for vn in self.Vn:
             self.isempty[vn] = None
             self.first[vn] = []
             self.follow[vn] = []
+            self.follow[self.start] = ['#']
+            self.select[vn] = {}
+
+        for pro in self.production:
+            self.select[left(pro)][right(pro)] = []
 
     def begin_judge(self):
         dic_map = {}
@@ -70,12 +87,58 @@ class LL1:
             self.__first_priv(self.production[i])
 
     def __follow_priv(self, head):
-        pass
+        fin = head
+        for pro in self.production:
+            try:
+                index = right(pro).index(fin)
+            except:
+                index = -1
+            if index == -1:
+                continue
+            if index + 1 == len(right(pro)):
+                self.follow[fin].append('Follow(' + left(pro) + ')')
+            elif right(pro)[index + 1] in self.Vt:
+                if right(pro)[index + 1] not in self.follow[fin]:
+                    self.follow[fin].append(right(pro)[index + 1])
+            elif right(pro)[index + 1] in self.Vn:
+                for i in self.first[right(pro)[index + 1]]:
+                    if i != '$':
+                        self.follow[fin].append(i)
 
     def follow_solve(self):
-        for i in range(len(self.production)):
-            self.__first_priv(self.production[i])
+        for i in range(len(self.Vn)):
+            self.__follow_priv(self.Vn[i])
+        judic = {}
+        while 1:
+            if judic == self.follow:
+                break
+            judic = self.follow
+            for key in self.follow:
+                for str in self.follow[key]:
+                    ret = re.match('Follow...', str)
+                    if ret != None:
+                        k = str.split('(')[1].split(')')[0]
+                        for s in self.follow[k]:
+                            self.follow[key].append(s)
+        for key in self.follow:
+            for str in self.follow[key]:
+                ret = re.match('Follow...', str)
+                if ret != None:
+                    self.follow[key].remove(str)
 
+    def __select_priv(self, head):
+        fin = left(head)
+        ter = right(head)
+        if ter[0] in self.Vt:
+            self.select[fin][ter].append(ter[0])
+        elif ter[0] == '$':
+            self.select[fin][ter] = list(set(self.select[fin][ter]).union(set(self.follow[fin])))
+        elif ter[0] in self.Vn:
+            pass
+
+    def select_solve(self):
+        for i in range(len(self.production)):
+            self.__select_priv(self.production[i])
 
 if __name__ == '__main__':
     a = LL1()
@@ -83,6 +146,13 @@ if __name__ == '__main__':
     a.init()
     a.empty()
     a.first_solve()
-
-    print(a.first)
+    a.follow_solve()
+    a.select_solve()
+    print('Empty:', end=" ")
     print(a.isempty)
+    print('First:', end=" ")
+    print(a.first)
+    print('Follow:', end=" ")
+    print(a.follow)
+    print('Select:', end=" ")
+    print(a.select)
